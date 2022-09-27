e327f1252d4f97893634404b9b4f227c8c92b6b2
Ankitsehrawat01 <sehrawata328@gmail.com>
2022-09-27 15:44:29 +0530

    Product review management program

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿namespace ProductReviewManagement
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Product Review Management Program");
+            List<product> productreviewlist = new List<product>()
+            {
+                new product() { ProductID = 1, Userid = 1, rating = 5, Review = "Good", Islike = true },
+                new product() { ProductID = 2, Userid = 2, rating = 5, Review = "fine", Islike = false },
+                new product() { ProductID = 3, Userid = 3, rating = 5, Review = "very good", Islike = true },
+                new product() { ProductID = 4, Userid = 4, rating = 5, Review = "bad", Islike = false },
+                new product() { ProductID = 5, Userid = 5, rating = 5, Review = "Excellent", Islike = true },
+                new product() { ProductID = 6, Userid = 6, rating = 5, Review = "Wrost", Islike = false },
+                new product() { ProductID = 7, Userid = 7, rating = 5, Review = "Good", Islike = true },
+                new product() { ProductID = 8, Userid = 8, rating = 5, Review = "Bad", Islike = true },
+                new product() { ProductID = 9, Userid = 9, rating = 5, Review = "Very Good", Islike = false },
+                new product() { ProductID = 10, Userid = 10, rating = 5, Review = "Excellent", Islike = true }
+            };
+            foreach (var products in productreviewlist)
+            {
+                Console.WriteLine("------------------------------------");
+                Console.WriteLine(products.ProductID + " | " + products.Userid + " | " + products.rating + " | " + products.Review + " | " + products.Islike);
+            }
+        }
+    }
+}
---
+++ product.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductReviewManagement
+{
+    public class product
+    {
+        public int ProductID { get; set; }
+        public int Userid { get; set; }
+        public int rating { get; set; }
+        public string Review { get; set; }
+        public bool Islike { get; set; }
+    }
+}
